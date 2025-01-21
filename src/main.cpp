6ae83cf6126db7fb8ff7d0816344014aa035bdea
Grim <75365746+Grim-Raven@users.noreply.github.com>
2025-01-21 09:59:15 +0100

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,567 @@
+/**
+   * @file main.cpp
+   * @author Victor Alessi
+   * @brief  This is the master device code. Encompasses master bt functionalities,
+   * sd card data formatting and saving, speed sensor readings, and MPU readings
+   * @version 0.1
+   * @date 2024-03-22
+   *
+   * @copyright Copyright (c) 2024
+   *
+   */
+
+#include <Arduino.h>
+#include <SD.h>
+#include "BluetoothSerial.h"
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <math.h>
+
+
+/******************************************************************************
+ *                                   DEFINES                                  *
+ ******************************************************************************/
+
+//############################ HALL SENSOR ###################################
+/**
+ * @brief
+ *     Minimum frequency accepted for the measurement of wheel speed, in Hertz.
+*/
+#define MIN_FREQUENCY                        0.1
+/**
+ * @brief
+ *     10^-6 micro preffix constant
+*/
+#define MICRO_CONSTANT                       1.0E-6
+
+/**
+ * @brief
+ *      Radius of the wheel
+*/
+#define RADIUS 50E-2
+
+/**
+ * @brief
+ *      Serial communication baudrate
+*/
+#define BAUDRATE 115200
+
+//############################ MPU  SENSOR ###################################
+/**
+ * @brief MPU DEFINITIONS
+ *
+ */
+Adafruit_MPU6050 mpu;
+
+#define MAX_BUFFER_SIZE 256
+#define MAX_BUFFER_SIZE_6 1550
+
+#if !defined(CONFIG_BT_SPP_ENABLED)
+  #error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+//############################ SD CARD     ###################################
+/**
+ * @brief SD SETUP
+ *
+ */
+#define CS_PIN 5 /**< Chip select pin for SD card */
+#define PIN_LED_LEFT_L2 33
+#define PIN_BUZZER 2
+
+/******************************************************************************
+ *                         LOCAL FUNCTION PROTOTYPES                          *
+ ******************************************************************************/
+// ############################ HALL SENSOR ###################################
+/**
+ * @brief
+ *  Interrupt handler function, responsible for obtaining average period readings.
+ *
+*/
+void IRAM_ATTR pulse_event();
+
+//############################ MPU  SENSOR ###################################
+// Prototypes
+void SlaveConnect(); /**< Function to connect to the slave device */
+void check_bt_connection(bool stat);
+void Bt_Status(esp_spp_cb_event_t event, esp_spp_cb_param_t* param); /**< Callback function for Bluetooth status */
+
+//############################ SD CARD     ###################################
+void data_logging(); /**< Function to log data */
+void alarm_system(float[]); /**<Function to start the alarm system*/
+
+
+/******************************************************************************
+ *                             GLOBAL VARIABLES                             *
+ ******************************************************************************/
+// ############################ HALL SENSOR ###################################
+/** @brief
+ *    Maximum period accepted for the measurement of wheel speed, defined by
+ * 1/MIN_FREQUENCY. In seconds
+ */
+float max_period = 1 / MIN_FREQUENCY;
+
+float measurement_runtime = 0;
+float runtime = 0;
+float time_between_pulses = 0;
+
+
+float angular_speed = 0;
+float linear_speed = 0;
+
+
+uint8_t pulse_counter = 0;
+uint8_t pulse_counter_last = 0;
+
+bool first_cycle = 1;
+uint8_t j = 0;
+//############################ MPU  SENSOR ###################################
+/**
+ * @brief BLUETOOTH SETUP
+ *
+ */
+const long interval = 1000;
+int ledState = LOW;
+unsigned long previousMillisLED = 0;
+unsigned long previousMillisReconnect; /**< Variable used for comparing millis counter for the reconnection timer */
+bool ledBtState = false; /**< Variable used to change the indication LED state */
+bool SlaveConnected; /**< Variable used to store the current connection state (true=connected/false=disconnected) */
+int recatt = 0; /**< Variable used to count the reconnection attempts */
+
+String myName = "ESP32-BT-Master";
+/**< Variable used to store the SERVER(Master) bluetooth device name; just for printing */
+String slaveName = "ESP32-BT-Slave";
+/**< Variable used to store the CLIENT(Slave) bluetooth device name; just for printing */
+String MACadd = "64:B7:08:29:35:72";
+/**< Variable used to store the CLIENT(Slave) bluetooth device Mac address; just for printing */
+uint8_t address[6] = {0x64, 0xB7, 0x08, 0x29, 0x35, 0x72};
+/**< Variable used to store the CLIENT(Slave) MAC address used for the connection */
+
+BluetoothSerial SerialBT; /**< Bluetooth Serial Object */
+
+char message_hall[MAX_BUFFER_SIZE];
+char buffer_hall[MAX_BUFFER_SIZE];
+char buffer_angular[MAX_BUFFER_SIZE];
+char buffer_linear[MAX_BUFFER_SIZE];
+
+//############################ SD CARD     ###################################
+File myFile; /**< File object for SD card */
+int i = 0; /**< Counter variable */
+
+// Déclaration des variables contenant les valeurs reçus.
+float acc_X_received;
+float acc_Y_received;
+float acc_Z_received;
+float acc_X_sensor;
+float acc_Y_sensor;
+float acc_Z_sensor;
+
+// Déclaration des variables contenant la différence entre les angles (pitch et yaw) mesurés et reçus.
+float initial_diff_Y;
+float initial_diff_Z;
+float diff_acc_X;
+float diff_acc_Y;
+float diff_acc_Z;
+
+// Compteur de seconde afin de connaitre le temps d'inattention (angle du casque > ANGLE)
+bool cpt_second = false;
+char receivedBuffer[1550];
+int bufferIndex = 0;
+char receivedChar2;
+char* token;
+const char t[2] = ",";
+// Define buffers as character arrays
+char message[MAX_BUFFER_SIZE_6]; // used for displaying on the serial monitor
+char buffer[MAX_BUFFER_SIZE_6]; // used to concatenate the data and send the message as a single package
+char buffer_acceleration_X[MAX_BUFFER_SIZE];
+char buffer_acceleration_Y[MAX_BUFFER_SIZE];
+char buffer_acceleration_Z[MAX_BUFFER_SIZE];
+char buffer_gyro_X[MAX_BUFFER_SIZE];
+char buffer_gyro_Y[MAX_BUFFER_SIZE];
+char buffer_gyro_Z[MAX_BUFFER_SIZE];
+
+void setup()
+{
+    /**
+     * @brief Hall sensor setup
+     *
+     */
+    pinMode(25, INPUT_PULLUP);
+    attachInterrupt(25, pulse_event, RISING); //   Enable interruption pin 25 when going from LOW to HIGH.
+
+    /**
+     * @brief Bluetooth setup
+     *
+     */
+
+    SlaveConnected = false; //  Set the variable false = CLIENT is not connected
+    Serial.begin(115200); //  Sets the data rate in bits per second (baud) for serial data transmission
+
+    SerialBT.register_callback(Bt_Status); // Define the Bt_Status callback
+    SerialBT.begin(myName, true); //   Starts the bluetooth device as SERVER(Master)
+    Serial.printf("The device \"%s\" started in master mode, make sure slave BT device is on!\n", myName.c_str());
+    SlaveConnect(); // Connect to the CLIENT(Slave)
+
+    /**
+     * @brief SD setup
+     *
+     */
+
+    pinMode(CS_PIN, OUTPUT);
+    pinMode(PIN_LED_LEFT_L2, OUTPUT);
+    pinMode(PIN_BUZZER, OUTPUT);
+
+    Serial.print("Initializing SD card... ");
+
+    if (!SD.begin(CS_PIN))
+    {
+        Serial.println("Card initialization failed!");
+        while (1); //  Stop the program
+    }
+
+    Serial.println("Initialization done.");
+
+    if (SD.exists("/example.csv"))
+    {
+        Serial.println("example.csv exists.");
+    }
+    else
+    {
+        Serial.println("example.csv doesn't exist.");
+    }
+
+    Serial.println("Creating example.csv...");
+    myFile = SD.open("/example.csv", FILE_WRITE);
+    myFile.print(
+        "slave_ax,slave_ay,slave_az,slave_gx,slave_gy,slave_gz,master_ax,master_ay,master_az,master_gx,master_gy,master_gz,angular_velocity,linear_velocity\n");
+    myFile.close();
+
+    if (SD.exists("/example.csv"))
+    {
+        Serial.println("example.csv exists.");
+    }
+    else
+    {
+        Serial.println("example.csv doesn't exist.");
+    }
+
+    /**
+     * @brief MPU Setup
+     *
+     */
+    if (!mpu.begin())
+    {
+        Serial.println("Failed to find MPU6050 chip");
+        while (1)
+        {
+            delay(10);
+        }
+    }
+
+    Serial.println("MPU6050 Found!");
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+    mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+}
+
+void getAcc(float(&acc)[3]);
+
+void loop()
+{
+    // Serial.println(pulse_counter);
+    // //  Speed sensor routine
+    // if ((pulse_counter - pulse_counter_last) > 1)
+    // {
+    //     // Get current application runtime
+    //     runtime = micros();
+    //     time_between_pulses = runtime - measurement_runtime;
+    //     time_between_pulses = time_between_pulses * (1e-6);
+    //
+    //
+    //     // Ignore intervals if they are too long
+    //     if (time_between_pulses > max_period)
+    //     {
+    //         time_between_pulses = 0;
+    //         pulse_counter_last = pulse_counter;
+    //     }
+    //     else
+    //     {
+    //         angular_speed = 2 * PI / time_between_pulses;
+    //         linear_speed = RADIUS * angular_speed;
+    //         Serial.println(angular_speed);
+    //         Serial.println(linear_speed);
+    //         pulse_counter_last = pulse_counter;
+    //     }
+    // }
+    // else
+    // {
+    //     angular_speed = pulse_counter;
+    //     linear_speed = pulse_counter;
+    // }
+    // snprintf(buffer_angular, sizeof(buffer_angular), "%f", angular_speed);
+    // snprintf(buffer_linear, sizeof(buffer_linear), "%f", linear_speed);
+    // sprintf(buffer_hall, ",%s,%s", buffer_angular, buffer_linear);
+    // strcpy(message_hall, buffer_hall);
+    // Serial.println(message_hall);
+    float acc[3]; // Tableau contenant les valeurs de l'accélération de master
+    float gyro[3]; // Tableau contenant les valeurs de l'orientation de master
+    // Bluetooth - MPU routine
+    if (!SlaveConnected)
+    {
+        if (millis() - previousMillisReconnect >= 10000)
+        {
+            previousMillisReconnect = millis();
+            recatt++;
+            Serial.print("Trying to reconnect. Attempt No.: ");
+            Serial.println(recatt);
+            Serial.println("Stopping Bluetooth...");
+            SerialBT.end();
+            Serial.println("Bluetooth stopped !");
+            Serial.println("Starting Bluetooth...");
+            SerialBT.begin(myName, true);
+            Serial.printf("The device \"%s\" started in master mode, make sure slave BT device is on!\n",
+                          myName.c_str());
+            SlaveConnect();
+        }
+    }
+
+    // Get new sensor events with the readings
+    sensors_event_t a, g, temp;
+
+    if (SerialBT.available())
+    {
+        if (mpu.getEvent(&a, &g, &temp))
+        {
+            getAcc(acc);
+            // snprintf(buffer_acceleration_X, sizeof(buffer_acceleration_X), "%f", a.acceleration.x);
+            // snprintf(buffer_acceleration_Y, sizeof(buffer_acceleration_Y), "%f", a.acceleration.y);
+            // snprintf(buffer_acceleration_Z, sizeof(buffer_acceleration_Z), "%f", a.acceleration.z);
+            // snprintf(buffer_gyro_X, sizeof(buffer_gyro_X), "%f", g.gyro.x);
+            // snprintf(buffer_gyro_Y, sizeof(buffer_gyro_Y), "%f", g.gyro.y);
+            // snprintf(buffer_gyro_Z, sizeof(buffer_gyro_Z), "%f", g.gyro.z);
+            // acc_X_sensor = a.acceleration.x;
+            // acc_Y_sensor = a.acceleration.y;
+            // acc_Z_sensor = a.acceleration.z;
+            // sprintf(buffer, ",%s,%s,%s,%s,%s,%s", buffer_acceleration_X, buffer_acceleration_Y, buffer_acceleration_Z,
+            //         buffer_gyro_X, buffer_gyro_Y, buffer_gyro_Z);
+            // strcat(message, buffer);
+            SerialBT.write('A');
+        }
+        // Serial.write(SerialBT.read());
+        alarm_system(acc);
+        // data_logging();
+
+        //  Reset for next batch
+        // memset(message, 0, sizeof(message));
+        // memset(buffer, 0, sizeof(buffer));
+        // memset(buffer_acceleration_X, 0, sizeof(buffer_acceleration_X));
+        // memset(buffer_acceleration_Y, 0, sizeof(buffer_acceleration_Y));
+        // memset(buffer_acceleration_Z, 0, sizeof(buffer_acceleration_Z));
+        // memset(buffer_gyro_X, 0, sizeof(buffer_gyro_X));
+        // memset(buffer_gyro_Y, 0, sizeof(buffer_gyro_Y));
+        // memset(buffer_gyro_Z, 0, sizeof(buffer_gyro_Z));
+        // memset(buffer_angular, 0, sizeof(buffer_angular));
+        // memset(buffer_linear, 0, sizeof(buffer_linear));
+        // memset(buffer_hall, 0, sizeof(buffer_hall));
+        // memset(message_hall, 0, sizeof(message_hall));
+    }
+}
+
+/**
+ * @brief
+ *  Récupère les valeurs de l'accélération du MPU de master
+ * @param acc
+ *  Tableau contenant les valeurs de l'accélération de master
+ */
+void getAcc(float (&acc)[3])
+{
+    sensors_event_t a, g, temp;
+    if (mpu.getEvent(&a, &g, &temp))
+    {
+        acc[0] = a.acceleration.x;
+        acc[1] = a.acceleration.y;
+        acc[2] = a.acceleration.z;
+    }
+}
+
+
+void Bt_Status(esp_spp_cb_event_t event, esp_spp_cb_param_t* param)
+{
+    if (event == ESP_SPP_OPEN_EVT)
+    {
+        Serial.println("Client Connected");
+        SlaveConnected = true;
+        recatt = 0;
+    }
+    else if (event == ESP_SPP_CLOSE_EVT)
+    {
+        Serial.println("Client Disconnected");
+        SlaveConnected = false;
+    }
+}
+
+void SlaveConnect()
+{
+    Serial.println("Function BT connection executed");
+    Serial.printf("Connecting to slave BT device named \"%s\" and MAC address \"%s\" is started.\n", slaveName.c_str(),
+                  MACadd.c_str());
+    SerialBT.connect(address);
+}
+
+void data_logging()
+{
+    myFile = SD.open("/example.csv", FILE_APPEND);
+
+    if (myFile)
+    {
+        Serial.println("File opened successfully.");
+
+        while (SerialBT.available())
+        {
+            Serial.println("B");
+            char receivedChar = SerialBT.read(); //reads "%s,%s,%s,%s,%s,%s!"
+
+            Serial.print(receivedChar);
+
+            if (receivedChar != '!')
+            {
+                myFile.print(receivedChar);
+            }
+            else
+            {
+                myFile.print(message);
+                myFile.print(message_hall);
+                myFile.println();
+                break;
+            }
+        }
+        myFile.close();
+        Serial.println("File closed.");
+        SerialBT.write('A');
+    }
+    else
+    {
+        Serial.println("Error opening file.");
+    }
+}
+
+void alarm_system(float acc[3])
+{
+    bufferIndex = 0; // Réinitialiser l'index avant lecture
+    memset(receivedBuffer, 0, sizeof(receivedBuffer)); // Effacer le buffer
+
+    while (SerialBT.available())
+    {
+        Serial.println("C");
+        receivedChar2 = SerialBT.read();
+        if (receivedChar2 == '!')
+        {
+            receivedBuffer[bufferIndex] = '\0'; // Terminer la chaîne
+            break;
+        }
+        else if (bufferIndex < sizeof(receivedBuffer) - 1) // Vérifier les limites du tableau
+        {
+            receivedBuffer[bufferIndex] = receivedChar2; // Stocker le caractère
+            bufferIndex++;
+        }
+        else
+        {
+            Serial.println("Buffer overflow detected!"); // Détection de dépassement
+            break;
+        }
+    }
+
+    // while (SerialBT.available())
+    // {
+    //     Serial.println("D");
+    //     receivedChar2 = SerialBT.read();
+    //     if (receivedChar2 == '!') // Check for end of line character to indicate end of string
+    //     {
+    //         receivedBuffer[bufferIndex] = '\0'; //  Null-terminate the string
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         receivedBuffer[bufferIndex] = receivedChar2; // Store the character in the buffer
+    //         bufferIndex++;
+    //     }
+    // }
+    // we have one package of data
+    token = strtok(receivedBuffer, t);
+    acc_X_received = atoff(token);
+    token = strtok(NULL, t);
+    acc_Y_received = atoff(token);
+    token = strtok(NULL, t);
+    acc_Z_received = atoff(token);
+    // Calcul de la différence
+    diff_acc_X = acc[0] - acc_X_received;
+    diff_acc_Y = acc[1] - acc_Y_received;
+    diff_acc_Z = acc[2] - acc_Z_received;
+    if (initial_diff_Y == 0)
+        initial_diff_Y = diff_acc_Y;
+    if (initial_diff_Z == 0)
+        initial_diff_Z = diff_acc_Z;
+    /*Serial.print("ACC X RECEIVED: ");
+    Serial.print(acc_X_received);
+    Serial.print(" ACC Y RECEIVED:");
+    Serial.println(acc_Y_received);
+    Serial.print(" ACC Z RECEIVED: ");
+    Serial.print(acc_Z_received);
+    Serial.println("Diff acc X : ");
+    Serial.print(diff_acc_X);
+    Serial.print(" Diff acc Y : ");
+    Serial.print(diff_acc_Y);*/
+    Serial.print(" Diff acc Z :  ");
+    Serial.print(diff_acc_Z);
+    Serial.print(" Diff acc Z INIT :  ");
+    Serial.print(initial_diff_Z);
+
+    if ((abs(diff_acc_Z) - abs(initial_diff_Z)) > 0.7)
+        cpt_second = true;
+    else
+    {
+        cpt_second = false;
+        digitalWrite(PIN_LED_LEFT_L2, LOW);
+        noTone(PIN_BUZZER);
+    }
+
+    // Activation de l'alarme
+    if (cpt_second)
+    {
+        if (millis() - previousMillisLED >= interval)
+        {
+            // save the last time you blinked the LED
+            previousMillisLED = millis();
+
+            // if the LED is off turn it on and vice-versa:
+            if (ledState == LOW)
+            {
+                ledState = HIGH;
+            }
+            else
+            {
+                ledState = LOW;
+            }
+
+            // set the LED with the ledState of the variable:
+            digitalWrite(PIN_LED_LEFT_L2, ledState);
+        }
+        tone(PIN_BUZZER, 500);
+
+        cpt_second = false;
+    }
+    bufferIndex = 0;
+    memset(receivedBuffer, 0, sizeof(receivedBuffer));
+}
+
+/**
+ * @brief
+ *  Interrupt handler function, responsible for obtaining average period readings.
+ *
+*/
+void IRAM_ATTR pulse_event()
+{
+    pulse_counter++;
+    measurement_runtime = micros();
+}
---
+++ src/slave.cpp
@@ -0,0 +1,156 @@
+/**
+ * @file main.cpp
+ * @author Victor Alessi
+ * @brief This is the slave device code. Empompasses slave bt functionalities,
+ * MPU readings and sendind data.
+ * @version 0.1
+ * @date 2024-03-22
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <Arduino.h>
+#include "BluetoothSerial.h"
+
+/**
+ * @brief MPU DEFINITIONS
+ *
+ */
+Adafruit_MPU6050 mpu;
+
+// Counters and accumulators for averaging
+int sample_count = 0;
+float sum_ax = 0, sum_ay = 0, sum_az = 0;
+float sum_gx = 0, sum_gy = 0, sum_gz = 0;
+
+/**
+ * @brief BLUETOOTH DEFINITIONS
+ *
+ */
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+String device_name = "ESP32-BT-Slave"; // BT: Variable used to store the CLIENT(slave) bluetooth device name
+String MACadd = "64:B7:08:29:35:72";
+uint8_t address[6]; // BT: Variable used to store the CLIENT(Slave) MAC address used for the connection; Use your own andress in the same format
+
+#define MAX_BUFFER_SIZE 256
+#define MAX_BUFFER_SIZE_6 1550 //6*256+space for char
+
+// Define buffers as character arrays
+char message[MAX_BUFFER_SIZE_6]; // used for displaying on the serial monitor
+char buffer[MAX_BUFFER_SIZE_6];  // used to concatenate the data and send the message as a single package
+char buffer_acceleration_X[MAX_BUFFER_SIZE];
+char buffer_acceleration_Y[MAX_BUFFER_SIZE];
+char buffer_acceleration_Z[MAX_BUFFER_SIZE];
+char buffer_gyro_X[MAX_BUFFER_SIZE];
+char buffer_gyro_Y[MAX_BUFFER_SIZE];
+char buffer_gyro_Z[MAX_BUFFER_SIZE];
+bool ack_received = true;
+int i = 0;
+
+void setup(void)
+{
+  // SERIAL BEGIN
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10);
+
+  // MPU INIT
+  if (!mpu.begin())
+  {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1)
+    {
+      delay(10);
+    }
+  }
+
+  Serial.println("MPU6050 Found!");
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+
+  // BLUETOOTH INIT
+  esp_read_mac(address, ESP_MAC_BT);
+  SerialBT.begin(device_name); // Bluetooth device name
+  while (!SerialBT.connected(1000))
+  {
+    Serial.println("Waiting for connection...");
+  }
+  while (i < 6)
+  {
+    Serial.print(address[i], HEX);
+    Serial.print("\t");
+    i++;
+  }
+  Serial.println();
+
+  memset(message, 0, sizeof(message));
+  memset(buffer, 0, sizeof(buffer));
+  memset(buffer_acceleration_X, 0, sizeof(buffer_acceleration_X));
+  memset(buffer_acceleration_Y, 0, sizeof(buffer_acceleration_Y));
+  memset(buffer_acceleration_Z, 0, sizeof(buffer_acceleration_Z));
+  memset(buffer_gyro_X, 0, sizeof(buffer_gyro_X));
+  memset(buffer_gyro_Y, 0, sizeof(buffer_gyro_Y));
+  memset(buffer_gyro_Z, 0, sizeof(buffer_gyro_Z));
+}
+
+void loop()
+{
+  // Get new sensor events with the readings
+  sensors_event_t a, g, temp;
+
+  if (mpu.getEvent(&a, &g, &temp))
+  {
+    sum_ax += a.acceleration.x;
+    sum_ay += a.acceleration.y;
+    sum_az += a.acceleration.z;
+
+    sum_gx += g.gyro.x;
+    sum_gy += g.gyro.y;
+    sum_gz += g.gyro.z;
+
+    sample_count++;
+    Serial.println(ack_received);
+    if (sample_count >= 100 && ack_received) {
+      snprintf(buffer, sizeof(buffer), "%f,%f,%f,%f,%f,%f!",
+               sum_ax / sample_count, sum_ay / sample_count, sum_az / sample_count,
+               sum_gx / sample_count, sum_gy / sample_count, sum_gz / sample_count);
+      SerialBT.write((uint8_t *)buffer, strlen(buffer));
+      ack_received = false; // Attendre le prochain ACK
+
+      // Reset for next batch
+      memset(message, 0, sizeof(message));
+      memset(buffer, 0, sizeof(buffer));
+      memset(buffer_acceleration_X, 0, sizeof(buffer_acceleration_X));
+      memset(buffer_acceleration_Y, 0, sizeof(buffer_acceleration_Y));
+      memset(buffer_acceleration_Z, 0, sizeof(buffer_acceleration_Z));
+      memset(buffer_gyro_X, 0, sizeof(buffer_gyro_X));
+      memset(buffer_gyro_Y, 0, sizeof(buffer_gyro_Y));
+      memset(buffer_gyro_Z, 0, sizeof(buffer_gyro_Z));
+      sum_ax = 0;
+      sum_ay = 0;
+      sum_az = 0;
+      sum_gx = 0;
+      sum_gy = 0;
+      sum_gz = 0;
+      sample_count = 0;
+    }
+
+    // Vérifier si un ACK est reçu
+    if (SerialBT.available()) {
+      char ack = SerialBT.read(); // Lire un caractère depuis le buffer
+      if (ack == 'A') {           // 'A' représente un accusé de réception
+        ack_received = true;
+      }
+    }
+  }
+}
